67ef133d57c949c6cfef3ac78696c7fc86bd25a5
Simon Gehring <simon.gehring@fkie.fraunhofer.de>
2018-07-17 15:35:58 +0200

    Added single instance

--- run.sh
+++ run.sh
@@ -5,8 +5,16 @@ set -e
 TIMEOUT_HOURS=8
 
 TIMEOUT=$((60*60*$TIMEOUT_HOURS))
+
+# Number of workers (+1, the master slave)
+NUMBER_OF_MACHINES=11
+MEMORY_PER_MACHINE=10G
+CORES_PER_MACHINE=1
+
 COMPRESSION_LOGS=with-compression/
 NO_COMPRESSION_LOGS=without-compression/
+SINGLE_LOGS=single-instance/
+SUP_SINGLE_INSTANCE=
 
 # Code for a console countdown timer (from https://superuser.com/questions/611538/is-there-a-way-to-display-a-countdown-or-stopwatch-timer-in-a-terminal/611582)
 function countdown(){
@@ -19,6 +27,11 @@ function countdown(){
 
 echo "Starting script. It is now $(date)."
 
+
+echo "Deleting old logfiles..."
+sudo rm -rf logs
+mkdir logs
+mkdir logs/build
 
 echo "Building shared-uima-processor..."
 cd ../master-thesis-program
@@ -38,21 +51,21 @@ echo "Removing old containers (if applicable)."
 docker-compose -f compose-1m2s.yaml down --remove-orphans 1>logs/init-docker-compose-down.stdout.log \
 2>logs/init-docker-compose-down.stderr.log
 
+#echo "Starting benchmark UIMA-AS..."
+#export SUP_LOG_FILES=$UIMA_AS_LOGS
+#export SUP_SINGLE_INSTANCE=--uima-as
+#docker-compose -f compose-1m2s.yaml up --scale slave-two=0 -d 1>logs/uima-as-docker-compose-up.stdout.log \
+#2>logs/uima-as-docker-compose-up.stderr.log
 
-echo "Deleting old logfiles..."
-sudo rm -rf logs
-mkdir logs
-mkdir logs/build
-
+#exit(1)
 
 echo "Starting benchmark with compression..."
 export SUP_COMPRESSION_ALGORITHM=gehring.uima.distributed.compression.ZLib
 export SUP_LOG_FILES=$COMPRESSION_LOGS
-docker-compose -f compose-1m2s.yaml up --scale slave-two=11 -d 1>logs/compression-docker-compose-up.stdout.log \
+docker-compose -f compose-1m2s.yaml up --scale slave-two=$NUMBER_OF_MACHINES -d 1>logs/compression-docker-compose-up.stdout.log \
 2>logs/compression-docker-compose-up.stderr.log
 
 echo "Successfully started benchmark. Wait for $TIMEOUT_HOURS hours..."
-
 countdown $TIMEOUT
 
 echo "Successfully waited (yay). Removing (hopefully) idling containers..."
@@ -65,7 +78,7 @@ sudo find logs -name \*.jar -delete
 echo "Starting benchmark without compression..."
 export SUP_COMPRESSION_ALGORITHM=gehring.uima.distributed.compression.NoCompression
 export SUP_LOG_FILES=$NO_COMPRESSION_LOGS
-docker-compose -f compose-1m2s.yaml up --scale slave-two=11 -d 1>logs/no-compression-docker-compose-up.stdout.log \
+docker-compose -f compose-1m2s.yaml up --scale slave-two=$NUMBER_OF_MACHINES -d 1>logs/no-compression-docker-compose-up.stdout.log \
 2>logs/no-compression-docker-compose-up.stderr.log
 
 echo "Successfully started benchmark. Wait for $TIMEOUT_HOURS hours..."
@@ -78,6 +91,22 @@ docker-compose -f compose-1m2s.yaml down -v 1>logs/no-compression-docker-compose
 echo "Removing useless JAR files..."
 sudo find logs -name \*.jar -delete
 
+echo "Starting benchmark single instance..."
+export SUP_LOG_FILES=$SINGLE_LOGS
+export SUP_SINGLE_INSTANCE=--single
+export MEMORY_OF_SLAVE_MASTER=$(($MEMORY_PER_MACHINE*($NUMBER_OF_MACHINES+1)))
+export CORES_OF_SLAVE_MASTER=$(($CORES_PER_MACHINE*($NUMBER_OF_MACHINES+1)))
+docker-compose -f compose-1m2s.yaml up --scale slave-two=0 -d 1>logs/single-docker-compose-up.stdout.log \
+2>logs/single-docker-compose-up.stderr.log
+
+echo "Successfully started benchmark. Wait for $TIMEOUT_HOURS hours..."
+countdown $TIMEOUT
+
+echo "Successfully waited (yay). Removing (hopefully) idling containers..."
+docker-compose -f compose-1m2s.yaml down 1>logs/single-docker-compose-down.stdout.log \
+2>logs/single-docker-compose-down.stderr.log
+
+
 echo "Retrieving log files..."
 export OLD_WD=$(pwd)
 cd logs/${COMPRESSION_LOGS}slave-one/workspace/driver*
@@ -88,11 +117,20 @@ cd logs/${NO_COMPRESSION_LOGS}slave-one/workspace/driver*
 cp stdout $OLD_WD/logs/stdout-without-compression.log
 cp stderr $OLD_WD/logs/stderr-without-compression.log
 cd $OLD_WD
+cd logs/${SINGLE_LOGS}slave-one/workspace/driver*
+cp stdout $OLD_WD/logs/stdout-single-instance.log
+cp stderr $OLD_WD/logs/stderr-single-instance.log
+cd $OLD_WD
+
+
 
 echo "Formatting files..."
 egrep -v "INFO|WARN" logs/stdout-with-compression.log > logs/stdout-with-compression.min.log
 egrep -v "INFO|WARN" logs/stderr-with-compression.log > logs/stderr-with-compression.min.log
 egrep -v "INFO|WARN" logs/stdout-without-compression.log > logs/stdout-without-compression.min.log
 egrep -v "INFO|WARN" logs/stderr-without-compression.log > logs/stderr-without-compression.min.log
+egrep -v "INFO|WARN" logs/stdout-single-instance.log > logs/stdout-single-instance.min.log
+egrep -v "INFO|WARN" logs/stderr-single-instance.log > logs/stderr-single-instance.min.log
+
 
 echo "Done with the script. It is now $(date)."
